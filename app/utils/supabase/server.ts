7e8947990b5784838c580db5d730e6ce6ceae2e0
Abhinav Kumar <abhinav.webdj@gmail.com>
2024-12-08 13:38:23 +0530

    deploy cokie try again

--- app/utils/supabase/server.ts
+++ app/utils/supabase/server.ts
@@ -1,18 +1,19 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createClient = async () => {
-  const cookieStore = cookies();
-  
+export const createClient = () => {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value;
+          const cookieStore = cookies();
+          const cookie = cookieStore.get(name);
+          return cookie?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
+          const cookieStore = cookies();
           try {
             cookieStore.set({ name, value, ...options });
           } catch (error) {
@@ -20,6 +21,7 @@ export const createClient = async () => {
           }
         },
         remove(name: string, options: CookieOptions) {
+          const cookieStore = cookies();
           try {
             cookieStore.set({ name, value: "", ...options });
           } catch (error) {
