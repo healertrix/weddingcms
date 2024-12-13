6f01ee2bffe399a1f60540fac3a14741ba769eda
Abhinav Kumar <abhinav.webdj@gmail.com>
2024-12-14 01:22:46 +0530

    remove type  check in prod

--- next.config.ts
+++ next.config.ts
@@ -27,7 +27,11 @@ const nextConfig: NextConfig = {
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  }
+  },
+  typescript: {
+    // Disable TypeScript during production builds
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
